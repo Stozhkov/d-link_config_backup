697b92305b65391dab37a7aa62e8ae27d4330146
Stozhkov <dima@stozhkov.net>
2017-02-14 16:09:00 +0300

    nothing

--- backup.py
+++ backup.py
@@ -5,11 +5,19 @@ from subprocess import Popen, PIPE
 
 
 def do_backup_config(ip, community, type, tftp):
-    Popen("snmpset -v2c -c " + community + " " + ip + " 1.3.6.1.4.1.171.12.1.2.1.1.3.3 a " + tftp + " \
-        1.3.6.1.4.1.171.12.1.2.1.1.4.3 i\
-        2 1.3.6.1.4.1.171.12.1.2.1.1.5.3 s dlink/" + ip + "_" + str(datetime.date.today()) + ".cfg\
-        1.3.6.1.4.1.171.12.1.2.1.1.7.3 i 2 1.3.6.1.4.1.171.12.1.2.1.1.8.3 i 3",
-          shell=True, stdin=PIPE, stdout=PIPE).stdout.read().split()
+    if type in [17, 25]:
+        Popen("snmpset -v2c -c " + community + " " + ip + " 1.3.6.1.4.1.171.12.1.2.1.1.3.3 a " + tftp + " \
+            1.3.6.1.4.1.171.12.1.2.1.1.4.3 i 2 \
+            1.3.6.1.4.1.171.12.1.2.1.1.5.3 s dlink/" + ip + "_" + str(datetime.date.today()) + ".cfg \
+            1.3.6.1.4.1.171.12.1.2.1.1.7.3 i 2 \
+            1.3.6.1.4.1.171.12.1.2.1.1.8.3 i 3",
+              shell=True, stdin=PIPE, stdout=PIPE).stdout.read().split()
+    else:
+        Popen("snmpset -v2c -c " + community + " " + ip + " 1.3.6.1.4.1.171.12.1.2.18.1.1.3.3 a " + tftp + " \
+            1.3.6.1.4.1.171.12.1.2.18.1.1.5.3 s dlink/" + ip + "_" + str(datetime.date.today()) + ".cfg \
+            1.3.6.1.4.1.171.12.1.2.18.1.1.8.3 i 2 \
+            1.3.6.1.4.1.171.12.1.2.18.1.1.12.3 i 3",
+              shell=True, stdin=PIPE, stdout=PIPE).stdout.read().split()
 
 
 config = ConfigParser.ConfigParser()
@@ -19,13 +27,14 @@ db = MySQLdb.connect(host=config.get('database', 'host'),
                      user=config.get('database', 'user'),
                      passwd=config.get('database', 'passwd'),
                      db=config.get('database', 'db'))
-tftp = config.get('tftp', 'ip')
+ip_tftp_server = config.get('tftp', 'ip')
 
 cursor = db.cursor()
 
 cursor.execute("select ip, access_snmp_write, type from devices LIMIT 10")
 
 for row in cursor.fetchall():
-    do_backup_config(row[0], row[1], row[2], tftp)
+#    print row[0]; print row[1]; print row[2]
+    do_backup_config(row[0], row[1], row[2], ip_tftp_server)
 
 db.close()
