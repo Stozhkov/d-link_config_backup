ece28f9e6c99a92ee90e9d2630fa8a15cf6dd748
Stozhkov <dima@stozhkov.net>
2017-02-17 13:47:16 +0300

    Add loging functions and refactoring

--- backup.py
+++ backup.py
@@ -5,8 +5,27 @@ import MySQLdb
 import hashlib
 import datetime
 import ConfigParser
-from subprocess import Popen, PIPE
 from time import sleep
+from subprocess import Popen, PIPE
+
+
+def write_in_log(wil_message, wil_ip=''):
+
+    date_time = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%d"))
+
+    try:
+        wil_log_file = open(path_to_log_file, 'a')
+    except IOError as e:
+        print "Can not open log file. I/O Error({0}): {1}".format(e.errno, e.strerror)
+        exit(1)
+    else:
+        if wil_ip != '':
+            wil_result_message = date_time + " " + wil_ip + " " + wil_message + '\n'
+        else:
+            wil_result_message = date_time + " " + wil_message + '\n'
+
+        wil_log_file.write(wil_result_message)
+        wil_log_file.close()
 
 
 def do_backup_config(dbc_ip, dbc_community, dbc_type, dbc_tftp, dbc_file_name):
@@ -67,6 +86,7 @@ config.read('config.cfg')
 ip_tftp_server = config.get('tftp', 'ip')
 path_to_tftp_folder = config.get('tftp', 'path_to_tftp_folder')
 path_to_archive = config.get('archive', 'path_to_archive')
+path_to_log_file = config.get('log', 'path_to_log_file')
 host_name = config.get('database', 'host')
 user_name = config.get('database', 'user')
 password = config.get('database', 'passwd')
@@ -87,25 +107,34 @@ cursor.execute("SELECT `ip`, `access_snmp_write`, `devices`.`type`, `id` \
                     AND `devices_config`.`do_backup` = '1'")
 
 for row in cursor.fetchall():
-    file_name = row[0] + "_" + str(datetime.date.today()) + "_" + get_random_word() + ".cfg"
-    do_backup_config(row[0], row[1], row[2], ip_tftp_server, file_name)
+
+    ip_address = str(row[0])
+    snmp_community = str(row[1])
+    device_type = row[2]
+    device_id = str(row[3])
+
+    file_name = ip_address + "_" + str(datetime.date.today()) + "_" + get_random_word() + ".cfg"
+    do_backup_config(ip_address, snmp_community, device_type, ip_tftp_server, file_name)
 
     sleep(5)
 
     if os.path.isfile(path_to_tftp_folder + file_name):
-        md5_hash = get_md5_sum(path_to_tftp_folder + file_name)
+        md5_hash = str(get_md5_sum(path_to_tftp_folder + file_name))
 
-        if check_config(str(row[3]), str(md5_hash)):
-            print str(row[3]) + "Config file did not update science last time"
+        if check_config(device_id, md5_hash):
+            print device_id + " Config file was not updated in last time"
+            write_in_log("Config file was not updated in last time", ip_address)
             remove_file(file_name)
         else:
             move_file_to_archive(file_name)
             cursor.execute("insert into backup (devid, fname, hash)\
-                            VALUES(" + str(row[3]) + ", '" + str(file_name) + "', '" + str(md5_hash) + "')")
+                            VALUES(" + device_id + ", '" + file_name + "', '" + md5_hash + "')")
             db.commit()
             print "Config was updated..."
+            write_in_log("Config was updated...", ip_address)
     else:
         print "File not found"
+        write_in_log("File not found", device_id)
 
 db.commit()
 db.close()
